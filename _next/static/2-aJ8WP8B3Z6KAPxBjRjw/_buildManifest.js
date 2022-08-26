2bd6c769b709ba202bfc79f6e3d57086dd9db659
kado17 <kado17@users.noreply.github.com>
2022-08-26 08:33:05 +0000

    deploy: 61e3348b6a0d3a3b6d5016420ca7dbc0021c8f82

--- _next/static/2-aJ8WP8B3Z6KAPxBjRjw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-a2add92f3af77a86.js","static/chunks/pages/index-6cc13c91088ccb12.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
