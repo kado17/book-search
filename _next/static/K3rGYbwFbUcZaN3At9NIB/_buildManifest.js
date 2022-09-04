ba11f2a4b4b3c9351d9023069a3ea09c5fb5974d
kado17 <kado17@users.noreply.github.com>
2022-09-04 13:42:37 +0000

    deploy: bc13741c50453a4510f06540c008066fd4489c2b

--- _next/static/K3rGYbwFbUcZaN3At9NIB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-a2add92f3af77a86.js","static/chunks/pages/index-494498f141b3582b.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/U0IfVEEiUQpXvSKm8S-pn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-a2add92f3af77a86.js","static/chunks/pages/index-75aafcb5b02ba463.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
